167e97a454bb92ed8822115c82972f30ccff48b4
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-10 12:44:58 +0300

    Delete core.js

--- core.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('yesButton').addEventListener('click', function() {
-            document.getElementById('response').textContent = "Чудовий вибір! Твоя пригода починається...";
-            // Тут можна додати перехід до наступного етапу квесту (якщо він буде на цій же сторінці або на іншій).
-        });
-
-        document.getElementById('noButton').addEventListener('click', function() {
-            document.getElementById('response').textContent = "Що ж, відпочинь і повертайся, коли будеш готовий.";
-        });
