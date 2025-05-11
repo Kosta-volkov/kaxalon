b73f3072b3dcba0a80a0f752c368dd1959c3de04
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-11 22:03:36 +0300

    Create script.js

---
+++ game/level2_badgers/script.js
@@ -0,0 +1,15 @@
+document.getElementById('bypassOption').addEventListener('click', function() {
+    if (Math.random() > 0.5) {
+        window.location.href = '../level3_badgers/index.html';
+    } else {
+        window.location.href = '../../lose/index.html';
+    }
+});
+
+document.getElementById('calmOption').addEventListener('click', function() {
+    if (Math.random() > 0.3) {
+        window.location.href = '../level3_badgers/index.html';
+    } else {
+        window.location.href = '../../lose/index.html';
+    }
+});
