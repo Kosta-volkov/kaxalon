002cbd22ec15fa1a541e3aa88eaee27a3cc8df2f
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-11 22:08:03 +0300

    Create script.js

---
+++ game/level4_badgers/script.js
@@ -0,0 +1,7 @@
+document.getElementById('healOption').addEventListener('click', function() {
+    window.location.href = '../../win/index.html?ending=good';
+});
+
+document.getElementById('keepOption').addEventListener('click', function() {
+    window.location.href = '../../win/index.html?ending=neutral';
+});
