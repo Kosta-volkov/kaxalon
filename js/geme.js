ebb779973cf11ee3ab8a529c3ee1eaf4cfdd3aa2
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-11 20:55:36 +0300

    Create geme.js

---
+++ js/geme.js
@@ -0,0 +1,15 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    document.getElementById('response').textContent = "Чудовий вибір! Твоя пригода починається...";
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    document.getElementById('response').textContent = "Що ж, відпочинь і повертайся, коли будеш готовий.";
+});
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = 'dal.html';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    window.location.href = 'stop.html';
+});
