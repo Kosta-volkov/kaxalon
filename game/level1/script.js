aad497a17b8f29caa98c3309791c75c3141e2a6f
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-11 22:13:19 +0300

    Delete game directory

--- game/level1/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('badgersOption').addEventListener('click', function() {
-    window.location.href = '../level2_badgers/index.html';
-});
-
-document.getElementById('caveOption').addEventListener('click', function() {
-    window.location.href = '../level2_cave/index.html';
-});
