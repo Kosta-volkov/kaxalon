2c46b5749a8ed8f98033d1fe19cebeca7b75ca61
Kosta-volkov <volkovkonstantin244@gmail.com>
2025-05-11 21:53:03 +0300

    Create level1.js

---
+++ game/level1.js
@@ -0,0 +1,7 @@
+document.getElementById('badgersOption').addEventListener('click', function() {
+    window.location.href = 'level2_badgers.html';
+});
+
+document.getElementById('caveOption').addEventListener('click', function() {
+    window.location.href = 'level2_cave.html';
+});
